055625929a6479197117aed2bbacff42819b2fd3
Luis 🦎 <luis.6496@hotmail.com>
2022-01-27 23:01:20 -0600

    Agregando ejemplo de implementación de GenServer para aplicación de clima

--- metex/lib/metex.ex
+++ metex/lib/metex.ex
@@ -1,18 +1,2 @@
 defmodule Metex do
-  @moduledoc """
-  Documentation for `Metex`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Metex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
