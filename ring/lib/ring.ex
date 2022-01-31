792b4959044a8abbbc36c48439da8d91cf7cbd46
Luis 🦎 <luis.6496@hotmail.com>
2022-01-31 12:56:29 -0600

    Creating a list of process and link them

--- ring/lib/ring.ex
+++ ring/lib/ring.ex
@@ -1,18 +1,32 @@
 defmodule Ring do
-  @moduledoc """
-  Documentation for `Ring`.
-  """
+  def create_processes(n) do
+    1..n
+    |> Enum.map(fn _ -> spawn(fn -> loop() end) end)
+  end
 
-  @doc """
-  Hello world.
+  def loop do
+    receive do
+      {:link, link_to} when is_pid(link_to) ->
+        Process.link(link_to)
+        loop()
 
-  ## Examples
+      :crash ->
+        1 / 0
+    end
+  end
 
-      iex> Ring.hello()
-      :world
+  def link_processes(procs) do
+    link_processes(procs, [])
+  end
 
-  """
-  def hello do
-    :world
+  def link_processes([proc_1, proc_2 | rest], link_processes) do
+    send(proc_1, {:link, proc_2})
+    link_processes([proc_2 | rest], [proc_1 | link_processes])
+  end
+
+  def link_processes([proc | []], link_processes) do
+    first_process = link_processes |> List.last()
+    send(proc, {:link, first_process})
+    :ok
   end
 end
